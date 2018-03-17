8d9520baee6af65f5940c0b290e1aa41e5637fa3
Vadym Kovalenko <v.kovalenko@triangu.com>
2018-03-17 10:07:26 +0000

    install-docker.sh edited online with Bitbucket

--- Labs/docker/install-docker.sh
+++ Labs/docker/install-docker.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-sudo yum remove docker docker-common docker-selinux docker-engine
+sudo yum remove -y docker docker-common docker-selinux docker-engine
 
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum-config-manager -y --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-sudo yum install docker-ce
+sudo yum install -y docker-ce
 
 sudo systemctl start docker
 
